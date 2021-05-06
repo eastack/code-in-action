e661d1bf9313860c3fd16f176951d9a1b723907f
eastack <admin@eastack.me>
2021-05-06 23:10:57 +0800

    learn rust

---
+++ trpl/branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("Hello");
+    } else {
+        println!("World");
+    }
+}
---
+++ trpl/variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    const MAX_POINTS: u32 = 10_0000;
+    println!("The max points is: {}", MAX_POINTS);
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+    println!("The value of element is: {}", element);
+}
